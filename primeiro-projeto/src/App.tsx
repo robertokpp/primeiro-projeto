f45459eded84115606a828c9e0a179800bfecc09
Roberto Kennedy <roberto.kpp@gmail.com>
2026-04-16 22:55:05 -0500

    feat: refactor project structure and add new components

--- primeiro-projeto/src/App.tsx
+++ primeiro-projeto/src/App.tsx
@@ -1,8 +1,28 @@
-export default function App(){
+import { Header } from "./components/Header";
+import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
+
+import styles from "./app.module.css";
+
+export default function App() {
+  function handleRestartGame() {
+    alert("Reiniciar o jogo");
+  }
+
   return (
-    <div>
-      <h1>Hello word</h1>
-      <button>teste</button>
+    <div className={styles.container}>
+      <main>
+        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip="Linguagem de programação dinâmica" />
+
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
+      </main>
     </div>
-  )
+  );
 }
